c5ad7b8dd45c498ad6e85e22ba6bcf2708d64c32
谭鹏 <tanpengcd@gmail.com>
2019-04-30 17:36:25 +0800

    work poker + caps->ctrl

--- keyboards/alf/dc60/keymaps/peng/keymap.c
+++ keyboards/alf/dc60/keymaps/peng/keymap.c
@@ -17,8 +17,9 @@
 enum peng_layers{
     _MAC,
     _WIN,
-    _GAME,
+//    _GAME,
     _EXTRA
+//    _SWITCH
 };
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //for Mac
@@ -27,33 +28,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
                         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
                         KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_TRNS,    KC_RSFT    , KC_TRNS,
-                        KC_LCTL, KC_LALT,KC_LGUI,                    KC_TRNS, KC_SPACE, KC_TRNS,               KC_RGUI,   TG(_WIN),     KC_TRNS, MO(_EXTRA), TG(_GAME)
+                        KC_LCTL, KC_LALT,KC_LGUI,                    KC_TRNS, KC_SPACE, KC_TRNS,               KC_RGUI,   TG(_WIN),     KC_TRNS, MO(_EXTRA), KC_RCTL
                         ),
     //for WIN
     [_WIN] = LAYOUT_all(
-                     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    KC_TRNS,
-                     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-                     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-                     KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_TRNS,    KC_RSFT    ,KC_TRNS,
-                     KC_LCTL, KC_LGUI,KC_LALT,                    KC_TRNS, KC_SPACE, KC_TRNS,               KC_RALT,   TG(_WIN),     KC_TRNS, MO(_EXTRA),TG(_GAME)
-                     ),
+                        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    KC_TRNS,
+                        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+                        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+                        KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_TRNS,    KC_RSFT    ,KC_TRNS,
+                        KC_LCTL, KC_LGUI,KC_LALT,                    KC_TRNS, KC_SPACE, KC_TRNS,               KC_RALT,   TG(_WIN),     KC_TRNS, MO(_EXTRA),KC_RCTL
+                        ),
     //for game
-    [_GAME] = LAYOUT_all(
-                     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    KC_TRNS,
-                     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-                     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-                     KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_TRNS,    KC_RSFT    ,KC_TRNS,
-                     KC_LCTL, KC_TRNS,KC_LALT,                    KC_TRNS, KC_SPACE, KC_TRNS,         KC_RALT,         TG(_WIN),    KC_TRNS, MO(_EXTRA),TG(_GAME)
-                     ),
+//    [_GAME] = LAYOUT_all(
+//                     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    KC_TRNS,
+//                     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+//                     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+//                     KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_TRNS,    KC_RSFT    ,KC_TRNS,
+//                     KC_LCTL, KC_TRNS,KC_LALT,                    KC_TRNS, KC_SPACE, KC_TRNS,         KC_RALT,         TG(_WIN),    KC_TRNS, MO(_EXTRA),KC_RCTL
+//                     ),
 
-    //
+    //momentary layer for poker
     [_EXTRA] = LAYOUT_all(
                      KC_GRV , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL, KC_TRNS,
                      KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS,  KC_TRNS, KC_TRNS, KC_CALC, KC_TRNS, KC_INSERT, KC_TRNS, KC_PSCREEN, KC_SCROLLLOCK, KC_PAUSE, KC_TRNS,
                      KC_CAPS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGUP, KC_TRNS,
                      KC_TRNS, KC_LSFT, KC_TRNS, KC_TRNS, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, KC_VOLD, KC_VOLU, KC_MUTE, KC_PGDOWN, KC_TRNS, KC_TRNS, KC_TRNS,
-                     KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS,                   KC_TRNS,KC_TRNS, MO(_EXTRA), KC_TRNS
+                     KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS,           KC_TRNS,    KC_TRNS, MO(_EXTRA), KC_TRNS
                      ),
+    //momentary layer for swich layer
+//    [_SWITCH] = LAYOUT_all(
+//                          KC_GRV , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL, KC_TRNS,
+//                          KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS,  KC_TRNS, KC_TRNS, KC_CALC, KC_TRNS, KC_INSERT, KC_TRNS, KC_PSCREEN, KC_SCROLLLOCK, KC_PAUSE, KC_TRNS,
+//                          KC_CAPS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGUP, KC_TRNS,
+//                          KC_TRNS, KC_LSFT, KC_TRNS, KC_TRNS, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, KC_VOLD, KC_VOLU, KC_MUTE, KC_PGDOWN, KC_TRNS, KC_TRNS, KC_TRNS,
+//                          KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS,                   MO(_SWITCH),KC_TRNS, MO(_EXTRA), KC_TRNS
+//                          ),
+    
+    
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
